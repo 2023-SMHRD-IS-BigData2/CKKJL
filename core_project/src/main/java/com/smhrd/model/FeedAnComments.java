0c34f7ea2d4941467aea8ea6e6f882c8fd7528f3
user <user@59.0.236.248>
2023-11-23 09:14:02 +0900

    수정

--- core_project/src/main/java/com/smhrd/model/FeedAnComments.java
+++ core_project/src/main/java/com/smhrd/model/FeedAnComments.java
@@ -1,6 +1,6 @@
 package com.smhrd.model;
 
-import java.util.Date;
+import java.sql.Date;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,10 +17,10 @@ import lombok.ToString;
 @ToString
 @RequiredArgsConstructor
 public class FeedAnComments {
-	@NonNull private boolean CoNum; // 댓글 및 대댓글 구분 (true = 댓글, false = 대댓글)
+	private boolean CoNum; // 댓글 및 대댓글 구분 (true = 댓글, false = 대댓글)
 	private int first_index; // 댓글 및 대댓글 고유번호
 	@NonNull private int second_index; // 주체가 되는 고유번호(댓글시 피드,대댓글시 댓글)
-	@NonNull private int user_index; // 작성자 고유번호
+	@NonNull private String user_index; // 작성자 고유번호
 	@NonNull private String content; // 작성글
 	private Date date; // 작성일시
 }
