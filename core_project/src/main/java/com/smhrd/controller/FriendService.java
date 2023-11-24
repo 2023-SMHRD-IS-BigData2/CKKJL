b9dcb699c4f10adfff1bf1405f88fd4d02e85ccf
SMHRD <SMHRD@119.200.31.136>
2023-11-24 12:10:53 +0900

    백섭

---
+++ core_project/src/main/java/com/smhrd/controller/FriendService.java
@@ -0,0 +1,23 @@
+package com.smhrd.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.smhrd.model.Member;
+
+
+@WebServlet("/FriendService")
+public class FriendService extends HttpServlet {
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		Member vo = request.getParameter("vo");
+		
+		
+	}
+
+}
