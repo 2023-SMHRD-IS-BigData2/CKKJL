3a222440f74b301794a23856185219571e3db154
user <user@59.0.236.248>
2023-11-28 07:43:11 +0900

    좋아요 서비스만 추가하면 긑

--- core_project/src/main/java/com/smhrd/model/FeedLike.java
+++ core_project/src/main/java/com/smhrd/model/FeedLike.java
@@ -2,13 +2,15 @@ package com.smhrd.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.ToString;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @ToString
 public class FeedLike {
-	@NonNull private int feed_index; // 피드 고유번호
-	@NonNull private String user_index; // 유저 고유번호
+	private int feed_index; // 피드 고유번호
+	private String user_index; // 유저 고유번호
 }
