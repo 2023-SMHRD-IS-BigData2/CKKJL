47133ab88e308a9de30d4cc7d8417f5fbb7d146c
smhrd <smhrd@59.0.129.16>
2023-11-29 09:23:21 +0900

    커밋

--- core_project/src/main/java/com/smhrd/model/Matching.java
+++ core_project/src/main/java/com/smhrd/model/Matching.java
@@ -18,6 +18,6 @@ public class Matching {
 	@NonNull private String writer;  // 글 작성자
 	@NonNull private String challenger;  // 매칭 신청자
 	@NonNull private int f_index;      // 매칭(Mercenary_Match) 고유번호
-	@NonNull private char matching;    // 상시 여부 OX
+	private char matching;    // 상시 여부 OX
 	
 }
